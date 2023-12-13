fa0988779ce05fc71f107163a4705cf9d1c70890
shay-t <tlemcanichayma@gmail.com>
2023-12-13 11:11:55 +0100

    test fonctionnel done

---
+++ coffee-machine/machine/src/test/java/fr/imt/shay/machine/cucumber/steps/CucumberStepsEspressoCoffeeMachineTest.java
@@ -0,0 +1,6 @@
+package fr.imt.shay.machine.cucumber.steps;
+
+public class CucumberStepsEspressoCoffeeMachineTest {
+
+
+}
--- coffee-machine/machine/src/test/java/fr/imt/shay/machine/cucumber/steps/CucumberStepsExpressoCoffeeMachineTest.java
+++
@@ -1,7 +0,0 @@
-package fr.imt.shay.machine.cucumber.steps;
-
-import io.cucumber.java.en.And;
-
-public class CucumberStepsExpressoCoffeeMachineTest {
-
-}
